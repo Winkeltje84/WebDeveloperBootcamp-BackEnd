38fdf9dd910ee9cd8df337355eaa149ae38e834e
Winkeltje84 <bwinkel@gmail.com>
2017-07-13 20:48:17 +0200

    added new route & console.log messages to get requests to show in console

--- 01-Basics/b-FirstExpressApp/app.js
+++ 01-Basics/b-FirstExpressApp/app.js
@@ -4,9 +4,15 @@ var app = express();
 
 // "/" => "hi there!"
 app.get("/", function(req, res){
-    res.send("hi there!");
+  console.log("someone visited the root")
+  res.send("hi there!");
+})
+
+app.get("/bye", function(req, res){
+  console.log("someone visited the /bye page")
+  res.send("and bye again... :-(");
 })
 
 app.listen("1111", function(){
   console.log("Server running on http://localhost:1111/");
-});
+})
