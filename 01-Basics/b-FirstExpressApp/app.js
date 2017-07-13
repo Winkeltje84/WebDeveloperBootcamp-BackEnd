e1173083bf83424c1788457d5158717e0bdbda29
Winkeltje84 <bwinkel@gmail.com>
2017-07-13 20:29:09 +0200

    console log now shows where the server is running

--- 01-Basics/b-FirstExpressApp/app.js
+++ 01-Basics/b-FirstExpressApp/app.js
@@ -7,4 +7,6 @@ app.get("/", function(req, res){
     res.send("hi there!");
 })
 
-app.listen("1111");
+app.listen("1111", function(){
+  console.log("Server running on http://localhost:1111/");
+});
