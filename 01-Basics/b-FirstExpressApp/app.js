c16827ee14fb309795e2d3dd47f8fdfcc2891448
Winkeltje84 <bwinkel@gmail.com>
2017-07-13 21:50:42 +0200

    adding route parameters

--- 01-Basics/b-FirstExpressApp/app.js
+++ 01-Basics/b-FirstExpressApp/app.js
@@ -5,12 +5,19 @@ var app = express();
 // "/" => "hi there!"
 app.get("/", function(req, res){
   console.log("someone visited the root")
-  res.send("hi there!");
+  res.send("Welcome to the animal page! Please use path '/animals/:kind' for your type of animal!");
 })
 
-app.get("/bye", function(req, res){
-  console.log("someone visited the /bye page")
-  res.send("and bye again... :-(");
+app.get("/animals/:kind", function(req, res){
+  var requested = req.params.kind;
+  console.log("someone visited the '/animals/:" + requested + "' path");
+  console.log(req.params)
+  res.send("Welcom to the "  + requested + " page!");
+})
+
+app.get("*", function(req, res){
+  console.log("someone tried to visit a none existing page")
+  res.send("Error! This page does not exist!")
 })
 
 app.listen("1111", function(){
