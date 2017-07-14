c7cc9eb0037f687885e36b3e52ba48bd70a4e2a1
Winkeltje84 <bwinkel@gmail.com>
2017-07-14 12:33:15 +0200

    setting up "/friends" page showing a list with all friend in array

--- 01-Basics/e-PostRequestsApp/app.js
+++ 01-Basics/e-PostRequestsApp/app.js
@@ -8,6 +8,11 @@ app.get("/", function(req, res){
   res.render("home");
 })
 
+app.get("/friends", function(req, res){
+  var friends = ["Matt", "Ewoud", "Dennis", "Lee", "Vanessa", "Sorry for the others but this list is getting too long..."]
+  res.render("friends", { friends });
+})
+
 app.listen("1111", function(){
   console.log("server running on localhost:1111");
 })
