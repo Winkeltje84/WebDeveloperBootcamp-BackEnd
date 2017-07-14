6faa4e363ad27f56d5b63404ac25cd7ee2e75852
Winkeltje84 <bwinkel@gmail.com>
2017-07-14 13:15:50 +0200

    added form to /friends page with POST request to /friend/add that takes input & shows it in response

--- 01-Basics/e-PostRequestsApp/app.js
+++ 01-Basics/e-PostRequestsApp/app.js
@@ -1,16 +1,24 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
+  console.log("someone visited home")
   res.render("home");
 })
 
 app.get("/friends", function(req, res){
+  console.log("someone visited /friends")
   var friends = ["Matt", "Ewoud", "Dennis", "Lee", "Vanessa", "Sorry for the others but this list is getting too long..."]
   res.render("friends", { friends });
+})
+
+app.post("/friend/add", function(req, res){
+  res.send("you have reached the post route, you would like to add " + req.body.new_friend)
 })
 
 app.listen("1111", function(){
