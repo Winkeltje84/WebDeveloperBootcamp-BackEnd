340576d94b304e619516948c58a4664a8b576aff
Winkeltje84 <bwinkel@gmail.com>
2017-07-14 10:26:31 +0200

    created route to sport.ejs & route parameters are now given to that ejs file

--- 01-Basics/d-Express-render-HTML-templates/app.js
+++ 01-Basics/d-Express-render-HTML-templates/app.js
@@ -6,6 +6,11 @@ app.get("/", function(req, res){
   res.render("home.ejs");
 })
 
+app.get("/sports/:sport", function(req, res){
+  var sport = req.params.sport;
+  console.log(sport)
+  res.render("sport.ejs", { sport: sport });
+})
 
 app.listen("1111", function(){
   console.log("server is listening at localhost:1111");
