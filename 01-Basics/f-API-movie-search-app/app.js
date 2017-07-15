1e38322ba10452f74efac63e2c7457b916a5c572
Winkeltje84 <bwinkel@gmail.com>
2017-07-15 15:51:42 +0200

    Rendering movie Titles of API search result in /results view

--- 01-Basics/f-API-movie-search-app/app.js
+++ 01-Basics/f-API-movie-search-app/app.js
@@ -13,11 +13,12 @@ app.get("/search", function(req, res){
   res.send("we use this page to get search request");
 })
 
-app.get("/result", function(req, res){
-  request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body){
+app.get("/results", function(req, res){
+  request("http://www.omdbapi.com/?s=amsterdam&apikey=thewdb", function(error, response, body){
     if(!error && response.statusCode == 200){
-      var results = JSON.parse(body)
-      res.send(results["Search"][0]);
+      var results = JSON.parse(body)["Search"];
+      console.log(results)
+      res.render("results", { data: results });
     }
   });
 })
