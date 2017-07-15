5f0a0dbe8c642e222f700a6ef50d474c5d237767
Winkeltje84 <bwinkel@gmail.com>
2017-07-15 15:13:44 +0200

    JSON.parse to convert string into JSON and getting the first [0] Search result

--- 01-Basics/f-API-movie-search-app/app.js
+++ 01-Basics/f-API-movie-search-app/app.js
@@ -16,7 +16,8 @@ app.get("/search", function(req, res){
 app.get("/result", function(req, res){
   request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body){
     if(!error && response.statusCode == 200){
-      res.send(body);
+      var results = JSON.parse(body)
+      res.send(results["Search"][0]);
     }
   });
 })
