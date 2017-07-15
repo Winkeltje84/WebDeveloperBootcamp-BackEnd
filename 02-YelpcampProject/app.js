0b0abb6d2a6e090f95b66f2cd615e99511c97e23
Winkeltje84 <bwinkel@gmail.com>
2017-07-15 23:19:59 +0200

    added bootstrap styling: container/jumbotron/row/col-..-../thumbnail/caption

--- 02-YelpcampProject/app.js
+++ 02-YelpcampProject/app.js
@@ -11,6 +11,10 @@ var campgrounds = [
   { name: "Salmon Creek", image: "https://amsterdam.jekuntmeer.nl/imagehandler/contenttop/ykOjC-7deugden.png"},
   { name: "Granite Hill'", image: "http://www.brouwerijhetij.nl/wp-content/uploads/2015/05/Logo-t-IJ_web.jpg"},
   { name: "Mountain Goat's Rest", image:"http://www.hop-in.nl/wp-content/uploads/2016/02/Brouwerij-De-Leckere.png"},
+  { name: "Amsterdam Awesome camp", image:"https://pbs.twimg.com/media/C9XyhBPXYAEdNN2.jpg"},
+  { name: "Dog's camp", image:"https://s-media-cache-ak0.pinimg.com/236x/1b/91/aa/1b91aa7822f52763f8fe1c0defea4088--tent-camping.jpg"},
+  { name: "Campfire", image:"http://orig04.deviantart.net/1c38/f/2009/157/d/8/burning_camper_by_ichigok63.jpg"},
+  { name: "Watercamp", image:"https://s-media-cache-ak0.pinimg.com/originals/7a/b2/88/7ab288fc21daf71f7fa73e6f87254887.jpg"}
 ]
 
 app.get("/", function(req, res){
