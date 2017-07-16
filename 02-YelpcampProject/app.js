8efe624394055dc3508dc9b4ddea7f1228050451
Winkeltje84 <bwinkel@gmail.com>
2017-07-16 22:11:25 +0200

    added mongoose, connected to (to be created if not there yet) database and CREATE FUNCTION that will add a new campground to the database when app.js starts up

--- 02-YelpcampProject/app.js
+++ 02-YelpcampProject/app.js
@@ -1,21 +1,33 @@
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-var campgrounds = [
-  { name: "Salmon Creek", image: "https://amsterdam.jekuntmeer.nl/imagehandler/contenttop/ykOjC-7deugden.png"},
-  { name: "Granite Hill'", image: "http://www.brouwerijhetij.nl/wp-content/uploads/2015/05/Logo-t-IJ_web.jpg"},
-  { name: "Mountain Goat's Rest", image:"http://www.hop-in.nl/wp-content/uploads/2016/02/Brouwerij-De-Leckere.png"},
-  { name: "Amsterdam Awesome camp", image:"https://pbs.twimg.com/media/C9XyhBPXYAEdNN2.jpg"},
-  { name: "Dog's camp", image:"https://s-media-cache-ak0.pinimg.com/236x/1b/91/aa/1b91aa7822f52763f8fe1c0defea4088--tent-camping.jpg"},
-  { name: "Campfire", image:"http://orig04.deviantart.net/1c38/f/2009/157/d/8/burning_camper_by_ichigok63.jpg"},
-  { name: "Watercamp", image:"https://s-media-cache-ak0.pinimg.com/originals/7a/b2/88/7ab288fc21daf71f7fa73e6f87254887.jpg"}
-]
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create(
+  { name: "Watercamp", image:"https://s-media-cache-ak0.pinimg.com/originals/7a/b2/88/7ab288fc21daf71f7fa73e6f87254887.jpg"},
+   function(err, campground){
+     if(err){
+       console.log(err);
+     } else {
+       console.log("Newly created campground: ");
+       console.log(campground);
+     }
+   }
+)
 
 app.get("/", function(req, res){
   console.log("GET ROOT visted")
