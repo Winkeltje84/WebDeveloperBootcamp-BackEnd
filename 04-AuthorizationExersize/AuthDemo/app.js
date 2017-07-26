6aff6f25a864cffb7e8a8c86f3e65c07d743c64b
Winkeltje84 <bwinkel@gmail.com>
2017-07-26 21:13:28 +0200

    adding Session (and encrypting/decrypting) functionality to app.js

--- 04-AuthorizationExersize/AuthDemo/app.js
+++ 04-AuthorizationExersize/AuthDemo/app.js
@@ -11,6 +11,17 @@ mongoose.connect("mongodb://localhost/auth_demo_app")
 var app = express();
 app.set('view engine', 'ejs');
 
+app.use(require("express-session")({
+  secret: "Bozenhoven28",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res){
   res.render("home");
 })
