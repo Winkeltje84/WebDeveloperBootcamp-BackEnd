ec12d651a2fab5fa4d5b20e7e21199f129c0518a
Winkeltje84 <bwinkel@gmail.com>
2017-07-26 20:53:49 +0200

    setting up UserSchema & including packages in app.js

--- 04-AuthorizationExersize/AuthDemo/app.js
+++ 04-AuthorizationExersize/AuthDemo/app.js
@@ -1,5 +1,11 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    localStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User                  = require("./models/user");
+
 mongoose.connect("mongodb://localhost/auth_demo_app")
 
 var app = express();
