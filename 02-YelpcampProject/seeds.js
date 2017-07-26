f2d8143295ca8462a892211958fa18a8cc8ccf19
Winkeltje84 <bwinkel@gmail.com>
2017-07-26 15:28:47 +0200

    added some data seeds that will be seeded into the DB after removing them first

--- 02-YelpcampProject/seeds.js
+++ 02-YelpcampProject/seeds.js
@@ -1,14 +1,53 @@
 var mongoose = require('mongoose');
 var Campground = require('./models/campgrounds');
 
+var data = [
+  {
+    name: "CampFire",
+    image: "http://media.spokesman.com/photos/2016/05/25/SRX_CAMPER_FIRE.jpg",
+    description: "Hot in here..."
+  },
+  {
+    name: "WaterCamp",
+    image: "https://nathanaelvitkus.files.wordpress.com/2014/02/165ae-camping-in-the-rain.jpg",
+    description: "some like it wet!"
+  },
+  {
+    name: "BearCamp",
+    image: "https://cdn-jpg2.theactivetimes.com/sites/default/files/u199306/shutterstock_78709645.jpg",
+    description: "offers free wake up service"
+  },
+  {
+    name: "DirtCamp",
+    image: "http://static.fjcdn.com/pictures/Stuck+in+the+mud+camping_abcd04_3835420.jpg",
+    description: "When dirt is your middlename"
+  }
+]
+
 function seedDB(){
+  // remove all campgrounds
   Campground.remove({}, function(err){
     if(err){
       console.log(err);
     } else {
       console.log("removed campgrounds");
+      // add some campgrounds in a callback function
+      data.forEach(function(seed){
+        Campground.create(seed, function(err, seedData){
+          if(err){
+            console.log("Could't creat campground:");
+            console.log(err);
+          } else {
+            console.log("Added a campground from seeds...");
+          }
+        });
+      })
     }
   })
+
+
 }
+
+
 
 module.exports = seedDB;
