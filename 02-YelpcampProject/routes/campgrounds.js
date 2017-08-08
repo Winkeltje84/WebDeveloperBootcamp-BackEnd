9021c348304f104a47ef59aa18aa75ccfc06f178
Winkeltje84 <bwinkel@gmail.com>
2017-08-08 11:45:24 +0200

    MIDDLEWARE added to prevent users to edit/delete each others campgrounds

--- 02-YelpcampProject/routes/campgrounds.js
+++ 02-YelpcampProject/routes/campgrounds.js
@@ -51,11 +51,12 @@ router.get("/new", isLoggedIn, function(req, res){
 })
 
 // GET SHOW (CAMPGROUND) --> show page of particular campground
-router.get("/:id", isLoggedIn, function(req, res){
+router.get("/:id", function(req, res){
   var id = req.params.id;
   Campground.findById(id).populate("comments").exec(function(err, foundCampground){
     if(err){
       console.log("Error, no such Campground found");
+      console.log(err);
     } else {
       console.log("User visits a Campground show page:");
       console.log(foundCampground);
@@ -65,19 +66,22 @@ router.get("/:id", isLoggedIn, function(req, res){
 })
 
 // GET EDIT (CAMPGROUND) --> show edit page of particular campground
-router.get("/:id/edit", isLoggedIn, function(req, res){
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
   id = req.params.id;
   console.log("GET /campgrounds/" + id + "/edit visited --> rendering edit page...")
   Campground.findById(id, function(err, foundCampground){
     if(err){
       console.log(err);
+      res.redirect("/campgrounds");
     } else {
+      // console.log(foundCampground);
       res.render("campgrounds/edit", { campground: foundCampground });
     }
-  })})
+  })
+})
 
 // POST EDIT (CAMPGROUND) --> submit the edit form of a specific campground
-router.put("/:id", isLoggedIn, function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
   var id = req.params.id;
   var campground = req.body.campground;
   console.log("Campground is being updated...");
@@ -93,7 +97,7 @@ router.put("/:id", isLoggedIn, function(req, res){
   })
 })
 
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
   var id = req.params.id;
   console.log("DEL /campgrounds/" + id + " --> deleting campground...");
   Campground.findByIdAndRemove(id, function(err){
@@ -108,26 +112,39 @@ router.delete("/:id", function(req, res){
   })
 })
 
-// app.delete("/blogs/:id", function(req, res){
-//   var id = req.params.id;
-//   console.log("DEL /blogs/" + id + " --> deleting blogpost...");
-//   Blog.findByIdAndRemove(id, function(err){
-//     if(err){
-//       console.log("An error occured, could not DELETE blog post");
-//       console.log(err);
-//     } else {
-//       console.log("Succesfully deleted blog post, redirecting to blogposts...");
-//       res.redirect("/blogs");
-//     }
-//   })
-// })
-
 //  MIDDLEWARE
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next){
+  // check if user is logged in
+  if(req.isAuthenticated()){
+    Campground.findById(req.params.id, function(err, foundCampground){
+      // console.log(foundCampground.user.id);
+      // console.log(req.user._id);
+      if(err){
+        console.log("checkCampgroundOwnership: there was an error with .findById --> redirecting 'back' to previous page...")
+        return res.redirect("back");
+      } else {
+        // campground id found: check if Campground author id is same as current user id
+        if(foundCampground.user.id.equals(req.user._id)){
+          console.log("checkCampgroundOwnership: user authorization succesful --> Middleware: 'return next()'")
+          next();
+        } else {
+          console.log("checkCampgroundOwnership: user not authorized --> redirecting 'back' to previous page");
+          res.redirect("back");
+        }
+      }
+    })
+  } else {
+    // if user is not logged in
+    console.log("checkCampgroundOwnership: user not logged in --> Redirect to login page...")
+    res.redirect("/login");
+  }
 }
 
 module.exports = router;
