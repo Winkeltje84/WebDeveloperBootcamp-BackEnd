ca48a1100031e47715cb03d470c6051681d4945e
Winkeltje84 <bwinkel@gmail.com>
2017-08-14 21:57:02 +0200

    added USER EDIT page for ADMIN --> can change Username & Admin status

--- 02-YelpcampProject/routes/users.js
+++ 02-YelpcampProject/routes/users.js
@@ -18,4 +18,40 @@ router.get("/", middleware.checkIfAdmin, function(req, res){
   })
 })
 
+// GET EDIT (USER) --> render the edit form for a specific user
+router.get("/:id/edit", middleware.checkIfAdmin, function(req, res){
+  var id = req.params.id;
+  console.log("GET /user/" + id + "/edit visited --> rendering edit page...");
+  User.findById(id, function(err, foundUser){
+    if(err){
+      console.log("error, user not found, redirecting to '/users'...");
+      req.flash("error", "error, user not found, redirecting to '/users'...")
+      res.redirect("/users");
+    } else {
+      console.log(foundUser);
+      res.render("users/edit", { user: foundUser });
+    }
+  })
+})
+
+
+router.put("/:id", middleware.checkIfAdmin, function(req, res){
+  var id = req.params.id;
+  var edited_user = {
+    username: req.body.user.username,
+    isAdmin: req.body.user.isAdmin
+  };
+  User.findByIdAndUpdate(id, edited_user, function(err, updatedUser){
+    if(err){
+      console.log("error, could not update user, redirecting to '/users'");
+      req.flash("error", "Oops, something went wrong, could not update user")
+      res.redirect("/users");
+    } else {
+      console.log("Succesfully updated user, rendering '/users'");
+      req.flash("succes", "Succesfully updated user");
+      res.redirect("/users");
+    }
+  })
+})
+
 module.exports = router;
